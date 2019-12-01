9d5ab95efcd5b44bd42a2d4ed35506324e2752f7
jingyugao <1121087373@qq.com>
2019-12-01 13:37:36 +0800

    reslover

--- test/main.c
+++ test/main.c
@@ -9,11 +9,9 @@ void f(void *arg) {
   for (int i = 0; i < 1000; i++) {
     // usleep(500 * 1000);
     num++;
-<<<<<<< HEAD
-    atomic_fetch_add(&atmicnum, 1);
-=======
     atomic_fetch_add(&atmicnum,1);
   }
+}
 
 int main() __asm__("_main_main");
 
@@ -24,10 +22,6 @@ int main() {
   }
 
   timeSleep(5 * Second);
-<<<<<<< HEAD
-  printf("ret :%d,%d,\n", num, atmicnum);
-=======
   printf("ret :%d,%d,\n", num,atmicnum);
->>>>>>> 06fe6b9aeb0b86db5b7265985c996c7b4ebe9ad2
   assert(num <= atmicnum);
 }
